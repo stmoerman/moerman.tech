3a6968f79e48d9f405aedcb06b40bee1140513a2
Stephan Moerman <stephan.moerman@viewnary.com>
2023-09-06 10:56:15 +0200

    Updated aria-label accessibility elements

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -17,6 +17,7 @@ export default function ThemeSwitch() {
 
   return (
     <button
+      aria-label="Toggle between dark and light mode"
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-50 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:bg-opacity-50"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
