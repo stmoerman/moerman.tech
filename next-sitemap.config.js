b1e10104094a7a3ef2164290a4be872712aa44d8
Stephan Moerman <stephan.moerman@viewnary.com>
2023-09-06 10:27:17 +0200

    Trying to change server-sitemap generation for next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.moerman.tech",
   generateRobotsTxt: true, // (optional)
   outDir: "./public",
-  exclude: ["./app/server-sitemap.xml/*"], // <= exclude here
+  exclude: ["./app/api/server-sitemap.xml/*"], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
       `${process.env.SITE_URL}/server-sitemap.xml`, // <==== Add here
