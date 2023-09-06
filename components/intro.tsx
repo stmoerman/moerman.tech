9c564f5b556f24aaa325f426545ae7dc2d52001d
Stephan Moerman <stephan.moerman@viewnary.com>
2023-09-06 20:11:13 +0200

    Optimized images

--- components/intro.tsx
+++ components/intro.tsx
@@ -24,9 +24,9 @@ export default function Intro() {
             <Image
               src="/stephan-moerman.png"
               alt="Stephan Moerman portrait"
-              width="192"
-              height="192"
-              quality="95"
+              width={96}
+              height={96}
+              quality={90}
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
